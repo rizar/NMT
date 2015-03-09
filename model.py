2db2871750c77b31e92c39a1626efe355b557e8f
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-03-09 09:10:46 -0400

    Add plotting, linear transform from hidden state to context, low-rank approximation of output layer

--- model.py
+++ model.py
@@ -14,8 +14,11 @@ from blocks.graph import ComputationGraph
 from blocks.initialization import IsotropicGaussian, Orthogonal, Constant
 from blocks.extensions import Printing
 from blocks.extensions.monitoring import TrainingDataMonitoring
+from blocks.extensions.saveload import SerializeMainLoop
+from blocks.extensions.plot import Plot
 
-from blocks.bricks import Tanh, Maxout, Linear, FeedforwardSequence
+from blocks.bricks import (Tanh, Maxout, Linear, FeedforwardSequence, MLP,
+                           Initializable)
 from blocks.bricks.lookup import LookupTable
 from blocks.bricks.recurrent import GatedRecurrent
 from blocks.bricks.sequence_generators import (
@@ -38,24 +41,30 @@ y_mask_t = y_mask.T
 
 # Encoder
 lookup = LookupTable(30000, 100, name='english_embeddings')
-linear = Linear(input_dim=100, output_dim=1000,
-                weights_init=IsotropicGaussian(0.1), biases_init=Constant(0))
+linear = Linear(input_dim=100, output_dim=1000)
 rnn_input = linear.apply(lookup.apply(x_t))
 
 encoder = GatedRecurrent(Tanh(), None, 1000, name='encoder')
 
 last_hidden_state = encoder.apply(rnn_input, rnn_input, rnn_input,
                                   mask=x_mask_t)[-1]
+context_transform = MLP(dims=[1000, 1000], activations=[Tanh()])
+context = context_transform.apply(last_hidden_state)
 
+
+class InitializableFeedforwardSequence(FeedforwardSequence, Initializable):
+    pass
 
 # The decoder
 readout = Readout(source_names=['states', 'feedback'],
                   readout_dim=30000,
                   emitter=SoftmaxEmitter(),
                   feedback_brick=LookupFeedback(30000, 100),
-                  post_merge=FeedforwardSequence(
+                  post_merge=InitializableFeedforwardSequence(
                       [Maxout(num_pieces=2).apply,
-                       Linear(input_dim=500).apply]),
+                       Linear(input_dim=500, output_dim=100,
+                              use_bias=False).apply,
+                       Linear(input_dim=100).apply]),
                   merged_dim=1000)
 
 sequence_generator = SequenceGenerator(
@@ -66,24 +75,29 @@ sequence_generator = SequenceGenerator(
 
 # Calculate the cost
 cost = sequence_generator.cost(outputs=y_t, mask=y_mask_t,
-                               states=last_hidden_state)
+                               states=context)
 cost = (cost * y_mask_t).sum() / y_mask_t.sum()
 cost.name = 'cost'
 
 # Initialization of the weights
+lookup.weights_init = IsotropicGaussian(0.1)
+linear.weights_init = IsotropicGaussian(0.1)
+linear.biases_init = Constant(0)
 encoder.weights_init = Orthogonal()
+context_transform.weights_init = IsotropicGaussian(0.1)
+context_transform.biases_init = Constant(0)
 sequence_generator.weights_init = IsotropicGaussian(0.1)
 sequence_generator.biases_init = Constant(0)
 sequence_generator.push_initialization_config()
 sequence_generator.transition.weights_init = Orthogonal()
-readout.post_merge.children[1].weights_init = IsotropicGaussian(0.1)
-readout.post_merge.children[1].biases_init = Constant(0)
-lookup.weights_init = IsotropicGaussian(0.1)
+readout.post_merge.weights_init = IsotropicGaussian(0.1)
+readout.post_merge.biases_init = Constant(0)
 
-encoder.initialize()
-sequence_generator.initialize()
 lookup.initialize()
 linear.initialize()
+encoder.initialize()
+context_transform.initialize()
+sequence_generator.initialize()
 
 # Set up training algorithm (standard SGD with gradient clipping)
 cg = ComputationGraph(cost)
@@ -98,7 +112,9 @@ main_loop = MainLoop(
     data_stream=masked_stream,
     extensions=[
         TrainingDataMonitoring([cost], after_every_batch=True),
-        Printing(after_every_batch=True)
+        Plot('En-Fr', channels=[['cost']], after_every_batch=True),
+        Printing(after_every_batch=True),
+        SerializeMainLoop('model.pkl', every_n_batches=2048)
     ]
 )
 main_loop.run()
