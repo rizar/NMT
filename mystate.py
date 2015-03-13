f0f0e6c4d95483b3c0df3f9a30b1faf8676c6b08
sebastien-j <jeasebas@iro.umontreal.ca>
2015-03-12 21:03:20 -0400

    Add state file

---
+++ mystate.py
@@ -0,0 +1,12 @@
+dict(
+algo = 'SGD',
+lr = 0.,
+minlr = -1.,
+bs = 64,
+sort_k_batches = 1,
+source = ['/data/lisa/exp/jeasebas/NMT/data/ntst1213.en_0.h5'],
+target = ['/data/lisa/exp/jeasebas/NMT/data/ntst1213.fr_0.h5'],
+use_infinite_loop = False,
+overwrite = 0,
+save_iter = 0,
+prefix = '/data/lisatmp3/jeasebas/nmt/encdec_600/rnned-long_')
