3afedafeefc3401a176b431ca0048c400368bd59
orhanf <orhan.firat@ceng.metu.edu.tr>
2015-06-05 15:45:29 -0400

    config changes

--- config.py
+++ config.py
@@ -79,44 +79,49 @@ def get_config_wmt15_fide_en():
     config['seq_len'] = 50
     config['enc_nhids_0'] = 1000
     config['enc_nhids_1'] = 1000
-    config['dec_nhids'] = 1200
+    config['dec_nhids'] = 1000
     config['enc_embed_0'] = 620
     config['enc_embed_1'] = 620
     config['dec_embed'] = 620
-    config['src_rep_dim'] = 5  # Annotation dim for src_selector
-    config['trg_rep_dim'] = 2  # Annotation dim for trg_selector
     config['representation_dim'] = 2000  # this is the joint annotation
                                          # dimension of encoders
-    config['saveto'] = 'multiEnc_FIDE_multiCG'
+    config['saveto'] = 'multiEnc_FIDE_multiCG_fast'
 
     # Optimization related
     config['batch_size_enc_0'] = 80
     config['batch_size_enc_1'] = 80
     config['sort_k_batches'] = 12
-    config['step_rule'] = 'AdaDelta'
+    config['step_rule'] = 'Adam'
+    config['learning_rate'] = 1e-4
     config['step_clipping'] = 5
     config['weight_scale'] = 0.01
     config['schedule'] = [24, 24]
+    config['save_accumulators'] = True  # algorithms' update step variables
+    config['load_accumulators'] = True  # be careful with this
+    config['exclude_encs'] = [True, True]
+    config['additional_excludes'] = \
+        ['/decoder/sequencegeneratorwithmulticontext/readout/lookupfeedbackwmt15/lookuptable.W']
 
     # Regularization related
     config['weight_noise_ff'] = False
     config['weight_noise_rec'] = False
     config['dropout'] = 1.0
+    config['drop_input'] = [0.1, 0.1]
 
     # Vocabulary/dataset related
-    basedir = '/data/lisatmp3/firatorh/nmt/wmt15/data/fide-en/'
+    basedir = '/data/lisatmp3/firatorh/nmt/wmt15/data/fideen-en/'
     config['stream'] = 'fide-en'
-    config['src_vocab_0'] = basedir + 'vocab.fi.pkl'
-    config['src_vocab_1'] = basedir + 'vocab.de.pkl'
-    config['src_data_0'] = basedir + 'all.tok.clean.seg.fi-en.fi'
-    config['src_data_1'] = basedir + 'all.tok.clean.de-en.de'
-    config['src_vocab_size_0'] = 40000
-    config['src_vocab_size_1'] = 40000
+    config['src_vocab_0'] = basedir + 'fi2en/vocab.fi.pkl'
+    config['src_vocab_1'] = basedir + 'de2en/vocab.de.pkl'
+    config['src_data_0'] = basedir + 'fi2en/all.tok.clean.seg.shuf2.fi-en.fi'
+    config['src_data_1'] = basedir + 'de2en/all.tok.clean.shuf.split.de-en.de'
+    config['src_vocab_size_0'] = 40001
+    config['src_vocab_size_1'] = 200000
 
-    config['trg_vocab'] = basedir + 'joint_vocab.en.52k.pkl'
-    config['trg_data_0'] = basedir + 'all.tok.clean.fi-en.en'
-    config['trg_data_1'] = basedir + 'all.tok.clean.de-en.en'
-    config['trg_vocab_size'] = 51896
+    config['trg_vocab'] = basedir + 'joint_vocab.sub.en.52k.pkl'
+    config['trg_data_0'] = basedir + 'fi2en/all.tok.clean.shuf2.fi-en.en'
+    config['trg_data_1'] = basedir + 'de2en/all.tok.clean.shuf.de-en.en'
+    config['trg_vocab_size'] = 51546
 
     config['unk_id'] = 1
 
@@ -130,14 +135,15 @@ def get_config_wmt15_fide_en():
     config['val_set_grndtruth_1'] = '/data/lisatmp3/jeasebas/nmt/data/wmt15/full/dev/tok/newstest2013.tok.en'
     config['val_set_out_1'] = 'multiEnc_FIDE_multiCG/validation_out_1.txt'
     config['output_val_set'] = True
-    config['beam_size'] = 2
+    config['beam_size'] = 12
 
     # Timing related
     config['reload'] = True
-    config['save_freq'] = 1000
+    config['save_freq'] = 5000
     config['sampling_freq'] = 13
-    config['bleu_val_freq'] = 5000
-    config['val_burn_in'] = 40000
+    config['bleu_val_freq'] = 10000
+    config['val_burn_in'] = 80000
+    config['finish_after'] = 10000000
 
     # Monitoring related
     config['hook_samples'] = 2
@@ -220,14 +226,14 @@ def get_config_wmt15_fideen_en():
     config['val_set_grndtruth_1'] = '/data/lisatmp3/jeasebas/nmt/data/wmt15/full/dev/tok/newstest2013.tok.en'
     config['val_set_out_1'] = 'multiEnc_FIDE_multiCG/validation_out_1.txt'
     config['output_val_set'] = True
-    config['beam_size'] = 2
+    config['beam_size'] = 12
 
     # Timing related
     config['reload'] = True
-    config['save_freq'] = 2000
+    config['save_freq'] = 5000
     config['sampling_freq'] = 13
-    config['bleu_val_freq'] = 5000
-    config['val_burn_in'] = 20000
+    config['bleu_val_freq'] = 15000
+    config['val_burn_in'] = 80000
     config['finish_after'] = 10000000
 
     # Monitoring related
